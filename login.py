b3d947d79aa055a5f417d216fbaab024a52c4b29
unknown <mohamadpanahinik@gmail.com>
2023-03-16 14:55:49 +0330

    remove bg of login

--- login.py
+++ login.py
@@ -7,6 +7,7 @@ class A(Tk):
 
     def main(self):
         self.geometry('400x510+750+300')
+        self.config(bg='white')
 
         #image
         self.logFrmImg = PhotoImage(file='image/loginFrm.png')
@@ -14,16 +15,20 @@ class A(Tk):
         self.logBtnImg = PhotoImage(file='image/loginBtn.png')
         self.eyeImg = PhotoImage(file='image/openEye.png')
 
-        self.logFrmBg=Label(self,image=self.logFrmImg)
-        self.logoImgLog=Label(self.logFrmBg,image=self.logoImg,bg='white')
-        self.e_userNameLog=Entry(self.logFrmBg,font=('arial',18),bd=0,justify=RIGHT)
-        self.e_passwordLog=Entry(self.logFrmBg,font=('arial',18),bd=0,justify=RIGHT)
-        self.showBtn=Button(self.logFrmBg,image=self.eyeImg,bd=0,activebackground='white')
-        self.b_enterBtn=Button(self.logFrmBg,image=self.logBtnImg ,activebackground='white',bd=0)
+        # self.logFrmBg=Label(self,image=self.logFrmImg,bg='white')
+        self.logoImgLog=Label(self,image=self.logoImg,bg='white')
+        self.l_userNameLog = Label(self,text=' : نام کاربری',font=('Lalezar',17),bg='white')
+        self.e_userNameLog=Entry(self,font=('arial',18),justify=RIGHT,bd=2,relief='solid')
+        self.l_passwordLog = Label(self,text=' : رمز عبور',font=('Lalezar',17),bg='white')
+        self.e_passwordLog=Entry(self,font=('arial',18),justify=RIGHT,bd=2,relief='solid',show='*')
+        self.showBtn=Button(self,image=self.eyeImg,bd=0,activebackground='white')
+        self.b_enterBtn=Button(self,image=self.logBtnImg ,activebackground='white',bd=0)
 
-        self.logFrmBg.grid(row=1,column=1)
+        # self.logFrmBg.grid(row=1,column=1)
         self.logoImgLog.place(x=135,y=10)
+        self.l_userNameLog.place(x=250,y=175)
         self.e_userNameLog.place(x=80,y=215)
+        self.l_passwordLog.place(x=270,y=275)
         self.e_passwordLog.place(x=80,y=316)
         self.showBtn.place(x=20,y=310)
         self.b_enterBtn.place(x=120,y=400)
@@ -31,6 +36,17 @@ class A(Tk):
         self.e_userNameLog.focus()
         self.e_userNameLog.bind('<Return>',lambda event : self.e_passwordLog.focus())
         self.e_passwordLog.bind('<Return>',lambda event : self.b_enterBtn.focus())
-    
+        self.showBtn.bind('<Button-1>',self.funcShow)
+        
+
+    def funcShow(self,event=None):
+        if self.e_passwordLog['show']=='*':
+            self.e_passwordLog['show']=''
+            self.eyeImg['file']='image/closeEye.png'
+        else:
+            self.e_passwordLog['show']='*'
+            self.eyeImg['file']='image/openEye.png'
+
+
 O = A()
 O.mainloop()
