13ae16385037a579ba3bf3bda284369358979109
unknown <mohamadpanahinik@gmail.com>
2023-03-17 21:09:53 +0330

    add list to addProduct.py

--- addProduct.py
+++ addProduct.py
@@ -1,17 +1,23 @@
 from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
 import sqlite3 as sql
+
+
 class A(Tk):
     def __init__(self):
         Tk.__init__(self)
+        self.style=ttk.Style()
+        self.lst=[]
+
         self.main()
-    
+        self.data_to_list()
+
     def main(self):
         self.fildsBgImg    = PhotoImage(file='image/fildsBg.png')
-        self.fildImg1      = PhotoImage(file='image/fildImg1.png')
-        self.fildImg2      = PhotoImage(file='image/imgSelectorBg.png')
-        self.fildImg3      = PhotoImage(file='image/fildImg3.png')
+        self.kalaImg      = PhotoImage(file='image/imgSelectorBg.png')
         self.listKalaBgImg = PhotoImage(file='image/listSkalaBg.png')
-        self.dropdownImg   = PhotoImage(file='image/dropdown.png')
+        self.searchBtnImg   = PhotoImage(file='image/searchBtnImg.png')
         self.h_sabtKalaImg = PhotoImage(file='image/headerSabtKala.png')
         self.addKalaBtnImg = PhotoImage(file='image/addKalaBtn.png')
 
@@ -20,25 +26,65 @@ class A(Tk):
 
         self.h_sabtKala    = Label(self,image=self.h_sabtKalaImg)
         self.l_productId   = Label(self,text=' : کد کالا',font=('Lalezar',17))
-        self.e_productId   = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=16,relief='solid')
+        self.e_productId   = Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
         self.l_productName = Label(self,text=' : نام کالا',font=('Lalezar',17))
-        self.e_productName = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=16,relief='solid')
+        self.e_productName = Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
         self.l_productType = Label(self,text=' : نوع کالا',font=('Lalezar',17))
-        self.e_productType = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=16,relief='solid')
+        self.c_productType = ttk.Combobox(self,width = 20 , font = ('B Koodak' , 12),state='readonly',
+                                            justify = 'right',values=["مواد خام", "کالای خریداری شده",
+                                            "کالای توید شده اولیه", "کالای تولید شده برای فروش"])
         self.l_groupType   = Label(self,text=' : نام گروه کالا',font=('Lalezar',17))
-        self.e_groupType   = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=16,relief='solid')
+        self.c_groupType   = ttk.Combobox(self,width = 20 , font = ('B Koodak' , 12),state='readonly',
+                                          justify = 'right',values=["فلزات", "مواد غذایی"])
+        self.c_productType.set("یک گزینه را انتخاب کنید")
+        self.c_groupType.set("یک گزینه را انتخاب کنید")
         self.l_description = Label(self,text=' : توضیحات',font=('Lalezar',17))
-        self.e_description = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=25,relief='solid')
+        self.e_description = Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=28,relief='solid')
         self.l_purchase    = Label(self,text=' : نقطه خرید',font=('Lalezar',17))
-        self.e_purchase    = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=16,relief='solid')
+        self.e_purchase    = Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
         self.l_imgSelector = Label(self,text='انتخاب تصویر',font=('Lalezar',17))
-        self.imgSelectorBg = Label(self,bg='#F3F3F3',image=self.fildImg2,cursor='hand2')
-        self.l_search      = Label(self,text=' : جستجو',font=('Lalezar',17))
-        self.e_search      = Entry(self,font=('arial',16),bd=1,justify=RIGHT,width=16,relief='solid')
-        self.b_dropdownbtn = Label(self,bg='#F3F3F3',image=self.dropdownImg)
+        self.imgSelectorBg = Label(self,bg='#F3F3F3',image=self.kalaImg,cursor='hand2',width=150,height=150)
+        self.e_search      = Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
+        self.b_search      = Button(self,bg='#F3F3F3',image=self.searchBtnImg,activebackground='#F3F3F3',bd=0,cursor='hand2')
         self.b_addKala     = Button(self,bg='#F3F3F3',image=self.addKalaBtnImg,activebackground='#F3F3F3',bd=0,cursor='hand2',command=self.funcAddKala)
         self.listKalaBg    = Label(self,bg='white',image=self.listKalaBgImg)
 
+        #list
+        self.listKala= ttk.Treeview(self,show='headings',height=8)
+
+        self.listKala['columns']=('Purchase','Category','Type','Name','id','row')
+        #columns
+        self.listKala.column('#0',width=0,stretch=NO)
+        self.listKala.column('Purchase',width=220,anchor=E)
+        self.listKala.column('Category',width=220,anchor=E)
+        self.listKala.column('Type',width=220,anchor=E)
+        self.listKala.column('Name',width=220,anchor=E)
+        self.listKala.column('id',width=200,anchor=E)
+        self.listKala.column('row',width=150,anchor=E)
+        #heading
+        self.listKala.heading('#0',text='',anchor=E)
+        self.listKala.heading('Purchase',text=' : گروه کالا',anchor=E)
+        self.listKala.heading('Category',text=' : نوع بسته بندی',anchor=E)
+        self.listKala.heading('Type',text=' : نوع کالا',anchor=E)
+        self.listKala.heading('Name',text=' : نام کالا',anchor=E)
+        self.listKala.heading('id',text=' : کد کالا',anchor=E)
+        self.listKala.heading('row',text=' : ردیف',anchor=E)
+        self.style.theme_use('clam')
+        self.style.configure("Treeview.Heading",font=('Lalezar', 18),
+                            padding=[0, 5, 15, 5],background='#474A56',
+                            foreground="white",bd=0,relief='raised'
+                            )
+        self.style.map("Treeview.Heading",
+            background=[('active','#686A75')])
+        self.style.configure("Treeview", highlightthickness=0, 
+                            height=150,
+                            bd=0, font=('AraFProgram', 16),
+                            background="white",foreground="black",
+                            rowheight = 35,fieldbackground="white"
+                            )
+        self.style.map("Treeview",
+            background=[('selected', '#7A8BA7')],
+            foreground=[('selected', 'white')])
         #___________
         # self.productIdBg   = Label(self,bg='#F3F3F3',image=self.fildImg1)
         # self.productNameBg = Label(self,bg='#F3F3F3',image=self.fildImg1)
@@ -50,24 +96,23 @@ class A(Tk):
 
         self.h_sabtKala.place    (x=580 , y=0)
         self.l_productId.place   (x=1245 , y=100)
-        self.e_productId.place   (x=1000 , y=105)
-        self.l_productName.place (x=855 , y=100)
+        self.e_productId.place   (x=985 , y=105)
+        self.l_productName.place (x=840 , y=100)
         self.e_productName.place (x=585 , y=100)
-        self.l_productType.place (x=1240 , y=180)
-        self.e_productType.place (x=1000 , y=180)
-        self.l_groupType.place   (x=815 , y=180)
-        self.e_groupType.place   (x=585 , y=180)
-        self.l_description.place (x=830 , y=250)
+        self.l_productType.place (x=1240 , y=178)
+        self.c_productType.place (x=985 , y=178)
+        self.l_groupType.place   (x=1200 , y=250)
+        self.c_groupType.place   (x=985 , y=250)
+        self.l_description.place (x=820 , y=250)
         self.e_description.place (x=475 , y=250)
-        self.l_purchase.place    (x=1215 , y=255)
-        self.e_purchase.place    (x=1000 , y=255)
+        self.l_purchase.place    (x=820 , y=180)
+        self.e_purchase.place    (x=585 , y=180)
         self.l_imgSelector.place (x=210 , y=260)
-        self.imgSelectorBg.place (x=200 , y=100)
-        self.l_search.place      (x=340 , y=370)
-        self.e_search.place      (x=130 , y=375)
-        self.b_dropdownbtn.place (x=85 , y=370)
+        self.imgSelectorBg.place (x=190 , y=100)
+        self.e_search.place      (x=245 , y=375)
+        self.b_search.place      (x=85 , y=370)
         self.b_addKala.place     (x=1120 , y=340)
-        self.listKalaBg.place    (x=85 , y=420)
+        self.listKala.place      (x=85 , y=420)
 
         #____
         # self.productIdBg.place   (x=1000 , y=80)
@@ -81,23 +126,44 @@ class A(Tk):
         #_____bind_____
         self.e_productId.focus()
         self.e_productId.bind     ('<Return>',lambda event : self.e_productName.focus())
-        self.e_productName.bind     ('<Return>',lambda event : self.e_productType.focus())
-        self.e_productType.bind ('<Return>',lambda event : self.e_groupType.focus())
-        self.e_groupType.bind    ('<Return>',lambda event : self.e_purchase.focus())
+        self.e_productName.bind     ('<Return>',lambda event : self.e_purchase.focus())
         self.e_purchase.bind     ('<Return>',lambda event : self.e_description.focus())
         self.e_description.bind     ('<Return>',lambda event : self.b_addKala.focus())
         self.b_addKala.bind     ('<Return>', self.funcAddKala)
+        self.imgSelectorBg.bind     ('<Button-1>', self.funcAddImg)
 
-        self.b_addKala.bind    ('<Enter>',lambda event : self.funcBtnHover(self.addKalaBtnImg,'image/addKalaBtnH.png'))
-        self.b_addKala.bind    ('<Leave>',lambda event : self.funcBtnHover(self.addKalaBtnImg,'image/addKalaBtn.png'))
+        self.e_search.insert(0,'جستجو کد کالا  ')
+        self.e_search.bind('<Button-1>',lambda event :self.e_search.delete(0,END))
+        self.b_addKala.bind('<Enter>',lambda event : self.funcBtnHover(self.addKalaBtnImg,'image/addKalaBtnH.png'))
+        self.b_addKala.bind('<Leave>',lambda event : self.funcBtnHover(self.addKalaBtnImg,'image/addKalaBtn.png'))
+        self.b_search.bind('<Enter>',lambda event : self.funcBtnHover(self.searchBtnImg,'image/searchBtnImgH.png'))
+        self.b_search.bind('<Leave>',lambda event : self.funcBtnHover(self.searchBtnImg,'image/searchBtnImg.png'))
 
+    def data_to_list(self):
+        self.count=0
+        self.con=sql.connect('mydb.db')
+        self.cur=self.con.cursor()
+        row=self.cur.execute('SELECT * FROM kala')
+        for i in row :
+            self.lst.append(i)
+        
+        for i in self.lst:
+            self.listKala.insert(parent='',index='end',iid=self.count,text='',
+                                 values=(i[4],i[3],i[2],i[1],i[0],str(self.count+1)))
+            self.count += 1
+
+    def funcAddImg(self,event=None):
+        self.img_name = filedialog.askopenfilename()
+        self.kalaImg['file']= self.img_name
+    
     def funcAddKala(self , event=None):
         self.productId=self.e_productId.get()
         self.productName=self.e_productName.get()
-        self.productType=self.e_productType.get()
-        self.groupType=self.e_groupType.get()
+        self.productType=self.c_productType.get()
+        self.groupType=self.c_groupType.get()
         self.purchase=self.e_purchase.get()
         self.description=self.e_description.get()
+        self.photo = self.covert_to_binary_data(self.img_name)
 
         self.e_productId.delete(0,END)
         self.e_productName.delete(0,END)
@@ -105,20 +171,24 @@ class A(Tk):
         self.e_groupType.delete(0,END)
         self.e_purchase.delete(0,END)
         self.e_description.delete(0,END)
+        self.kalaImg['file']='image/imgSelectorBg.png'
         self.e_productId.focus()
 
         self.con=sql.connect('mydb.db')
         self.cur=self.con.cursor()
-        self.data=(self.productId,self.productName,self.productType,self.groupType,self.purchase,self.description)
-        self.cur.execute('''CREATE TABLE IF NOT EXISTS kala (id PRIMARY KEY ,name TEXT ,type TEXT,category TEXT
-        ,purchase INTEGER,description TEXT)''')
-        self.cur.execute('INSERT INTO kala(id,name,type,category,purchase,description) VALUES(?,?,?,?,?,?)',self.data)
+        self.data=(self.productId,self.productName,self.productType,self.groupType,self.purchase,self.description,self.photo)
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS kala (id PRIMARY KEY NOT NULL ,name TEXT NOT NULL ,type TEXT NOT NULL,category TEXT NOT NULL
+        ,purchase INTEGER NOT NULL,description TEXT NOT NULL,photo BLOB NOT NULL)''')
+        self.cur.execute('INSERT INTO kala(id,name,type,category,purchase,description,photo) VALUES(?,?,?,?,?,?,?)',self.data)
         self.con.commit()
 
-
+    def covert_to_binary_data(self,filename):
+        with open (filename , 'rb') as f:
+            blobdata = f.read()
+        return blobdata
+    
     def funcBtnHover(self,img,url):
         img['file'] = url
-
 
 O=A()
 O.mainloop()
--- dfsd.py
+++ dfsd.py
@@ -1,97 +1,36 @@
 from tkinter import *
-root = Tk()
-loginpage = Toplevel()
-registerpage = Toplevel()
-#---------PagesBGs-Images---------------
-img1 = PhotoImage(file = "pics/login.png")
-img2 = PhotoImage(file = 'pics/mainpg.png')
-img3 = PhotoImage(file = 'pics/register-page.png')
-#---------Main-PageBtns-Images----------
-img4 = PhotoImage(file = 'pics/fish.png')
-img5 = PhotoImage(file = 'pics/darkhast.png')
-img6 = PhotoImage(file = 'pics/mojodi.png')
-img7 = PhotoImage(file = 'pics/resid.png')
-img8 = PhotoImage(file = 'pics/sabtKala.png')
-img9 = PhotoImage(file = 'pics/sabtKarbar.png')
-#---------login-PageBtns-Images----------
-img10 = PhotoImage(file = 'pics/login-btn.png')
-class app:
-    def __init__(self,event = None): 
-        self.MainPage()
-        self.loginPage()
-        self.registerPage()
-    def MainPage(self, event = None) :
-        root.geometry('1200x720+150+20')
-        root.state('withdraw')
-        self.main_lbl = Label(root, width = 1184 , height = 709 , bg = 'white' , image = img2 )
+from tkinter import ttk
 
-        self.fishBtn = Button(root, width = 157 , height  = 62 , image = img4 , activebackground= '#EEEEEE', borderwidth = 0)
-        self.fishBtn.place(x = 90 , y = 139)
+class A(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.main()
 
-        self.fishBtn.bind('<Enter>',lambda event : self.hoverBtn(img4,'pics/fish-hover.png'))
-        self.fishBtn.bind('<Leave>',lambda event : self.hoverBtn(img4,'pics/fish.png'))
-        
-        self.residBtn = Button(root, width = 157 , height  = 62 , image = img7 ,activebackground= '#EEEEEE', borderwidth = 0)
-        self.residBtn.place(x = 263 , y = 139)  
+    def main(self):
+        self.geometry('500x500')
+        self.my_tree= ttk.Treeview(self)
 
-        self.residBtn.bind('<Enter>',lambda event : self.hoverBtn(img7,'pics/resid-hover.png'))
-        self.residBtn.bind('<Leave>',lambda event : self.hoverBtn(img7,'pics/resid.png'))
+        self.my_tree['columns']=('name','id','favorite')
 
+        #columns
+        self.my_tree.column('#0',width=120,minwidth=25)
+        self.my_tree.column('name',width=120,anchor=W)
+        self.my_tree.column('id',width=80,anchor=CENTER)
+        self.my_tree.column('favorite',width=120,anchor=W)
 
-        self.darkhastBtn = Button(root, width = 157 , height  = 62 , image = img5,activebackground= '#EEEEEE', borderwidth = 0)
-        self.darkhastBtn.place(x = 436 , y = 139)
+        #heading
+        self.my_tree.heading('#0',text='label',anchor=W)
+        self.my_tree.heading('name',text='name',anchor=W)
+        self.my_tree.heading('id',text='id',anchor=CENTER)
+        self.my_tree.heading('favorite',text='fav',anchor=W)
 
-        self.darkhastBtn.bind('<Enter>',lambda event : self.hoverBtn(img5,'pics/darkhast-hover.png'))
-        self.darkhastBtn.bind('<Leave>',lambda event : self.hoverBtn(img5,'pics/darkhast.png'))
+        #add data to list
+        self.my_tree.insert(parent='',index='end',iid=0,text='',values=('ali','5','agha ali'))
+        self.my_tree.insert(parent='',index='end',iid=1,text='',values=('ali','5','agha ali'))
+        self.my_tree.insert(parent='',index='end',iid=2,text='',values=('ali','5','agha ali'))
+        self.my_tree.insert(parent='',index='end',iid=3,text='',values=('ali','5','agha ali'))
 
-        self.mojodiBtn = Button(root, width = 157 , height  = 62 , image = img6 , activebackground= '#EEEEEE', borderwidth = 0)
-        self.mojodiBtn.place(x = 609 , y = 139)
+        self.my_tree.place(x=0,y=0)
 
-        self.mojodiBtn.bind('<Enter>',lambda event : self.hoverBtn(img6,'pics/mojodi-hover.png'))
-        self.mojodiBtn.bind('<Leave>',lambda event : self.hoverBtn(img6,'pics/mojodi.png'))
-
-        self.sbtKarbarBtn = Button(root, width = 157 , height  = 63 , image = img9,activebackground= '#EEEEEE', borderwidth = 0)
-        self.sbtKarbarBtn.place(x = 782 , y = 139)
-
-        self.sbtKarbarBtn.bind('<Enter>',lambda event : self.hoverBtn(img9,'pics/sabtKarbar-hover.png'))
-        self.sbtKarbarBtn.bind('<Leave>',lambda event : self.hoverBtn(img9,'pics/sabtKarbar.png'))
-
-        self.sbtKalaBtn = Button(root, width = 157 , height  = 62 , image = img8,activebackground= '#EEEEEE', borderwidth = 0)
-        self.sbtKalaBtn.place(x = 955 , y = 136)
-
-        self.sbtKalaBtn.bind('<Enter>',lambda event : self.hoverBtn(img8,'pics/sabtKala-hover.png'))
-        self.sbtKalaBtn.bind('<Leave>',lambda event : self.hoverBtn(img8,'pics/sabtKala.png'))
-
-        self.main_lbl.place(x = 5 , y = 0)
-    def loginPage(self, event = None):
-        self.loginpage = loginpage
-        self.loginpage.state('normal')
-        self.loginpage.geometry('852x588+250+20')
-        self.login_main_lbl = Label(self.loginpage , width = 852 , height = 588 , bg = 'white')
-        self.login_main_lbl.place(x = 0 , y = 0)
-        self.login_lbl = Label(self.loginpage , width = 815 , height = 550 , bg = 'white' , image = img1 )
-        self.login_lbl.place(x = 15, y = 15)
-
-        self.login_btn = Button(self.loginpage , bg = 'white' , width = 211 , height = 65 , image = img10 ,activebackground= '#ffffff', borderwidth = 0)
-        self.login_btn.place( x = 528 , y = 376)
-
-        self.login_btn.bind('<Enter>',lambda event : self.hoverBtn(img10,'pics/login-btn-hover.png'))
-        self.login_btn.bind('<Leave>',lambda event : self.hoverBtn(img10,'pics/login-btn.png'))
-
-
-
-
-    def registerPage(self, event = None):
-        self.registerpage = registerpage
-        self.registerpage.state('withdraw')
-        self.registerpage.geometry('852x588+250+20')
-        self.register_lbl = Label(self.registerpage , width = 815 , height = 550 , bg = 'white' , image = img3 )
-        
-        self.register_lbl.place(x = 15, y = 15)
-
-    def hoverBtn(self,img,url):
-        img['file'] = url
-
-
-app = app(root)
-root.mainloop()
+O=A()
+O.mainloop()
