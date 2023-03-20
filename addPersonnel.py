c51821ff9990132833064a92655bf12eddae60c2
unknown <mohamadpanahinik@gmail.com>
2023-03-20 12:52:20 +0330

    function addkarmand

--- addPersonnel.py
+++ addPersonnel.py
@@ -11,15 +11,14 @@ class A(Tk):
 
 
     def main(self):
-        self.hSabtKalaImg = PhotoImage(file='image/headerSabtKarmandImg.png')
-        self.hSabtKalaImg = PhotoImage(file='image/headerSabtKarmandImg.png')
-        self.kalaImg = PhotoImage(file='image/imgSelectorBg.png')
+        self.hSabtkarmandImg = PhotoImage(file='image/headerSabtKarmandImg.png')
+        self.karmandImg = PhotoImage(file='image/imgSelectorBg.png')
         self.searchBtnImg   = PhotoImage(file='image/searchBtnImg.png')
         self.addPesonnelImg = PhotoImage(file='image/addPesonnelBtnImg.png')
         self.geometry ('1400x800+250+100')
         self.configure (bg='#F3F3F3')
     
-        self.l_headerKarmand=Label(self,image=self.hSabtKalaImg)
+        self.l_headerKarmand=Label(self,image=self.hSabtkarmandImg)
         self.l_nameKarmand=Label(self,text=' : نام',font=('Lalezar',17))
         self.e_nameKarmand=Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
         self.l_lastKarmand=Label(self,text=' : نام خانوادگی',font=('Lalezar',17))
@@ -41,7 +40,7 @@ class A(Tk):
         self.l_karmandPass=Label(self,text=' : رمزعبور',font=('Lalezar',17))
         self.e_karmandPass=Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
         self.l_imgSelector = Label(self,text='انتخاب تصویر',font=('Lalezar',17))
-        self.imgSelectorBg = Label(self,bg='#F3F3F3',image=self.kalaImg,cursor='hand2',width=150,height=150)
+        self.imgSelectorBg = Label(self,bg='#F3F3F3',image=self.karmandImg,cursor='hand2',width=150,height=150)
         self.b_addPesonnel= Button(self,bg='#F3F3F3',image=self.addPesonnelImg,activebackground='#F3F3F3',bd=0,cursor='hand2')
         self.e_searchKarmand   = Entry(self,font=('AraFProgram', 16),bd=1,justify=RIGHT,width=18,relief='solid')
         self.b_searchKarmand= Button(self,bg='#F3F3F3',image=self.searchBtnImg,activebackground='#F3F3F3',bd=0,cursor='hand2')
@@ -118,16 +117,57 @@ class A(Tk):
         self.e_nationalCode.bind('<Return>',lambda event : self.e_phoneNum.focus())
         self.e_phoneNum.bind('<Return>',lambda event : self.e_personnelId.focus())
         self.e_personnelId.bind('<Return>',lambda event : self.e_karmandPass.focus())
-        # self.e_karmandPass.bind('<Return>',lambda event : self.e_karmandPass.focus())
+        self.e_karmandPass.bind('<Return>',lambda event : self.e_karmandPass.focus())
+        self.e_karmandPass.bind('<Return>',lambda event : self.funcAddKarmand)
+        self.b_addPesonnel.bind('<Button-1>',self.funcAddKarmand)
         self.imgSelectorBg.bind('<Button-1>', self.funcAddImg)
 
 
     def funcAddImg(self,event=None):
         self.img_name = filedialog.askopenfilename()
-        self.kalaImg['file']= self.img_name
+        self.karmandImg['file']= self.img_name
 
     def funcBtnHover(self,img,url):
         img['file'] = url
+
+    def covert_to_binary_data(self,filename):
+        with open (filename , 'rb') as f:
+            blobdata = f.read()
+        return blobdata
+    
+    def funcAddKarmand(self , event=None):
+        self.pesonnelName=self.e_nameKarmand.get()
+        self.pesonnelLast=self.e_lastKarmand.get()
+        self.nationalCode=self.e_nationalCode.get()
+        self.gender=self.c_gender.get()
+        self.phoneNum=self.e_phoneNum.get()
+        self.accountType=self.c_accountType.get()
+        self.personnelId=self.e_personnelId.get()
+        self.personnelPass=self.e_karmandPass.get()
+        self.photo = self.covert_to_binary_data(self.img_name)
+
+        self.e_nameKarmand.delete(0,END)
+        self.e_lastKarmand.delete(0,END)
+        self.e_nationalCode.delete(0,END)
+        self.c_gender.set("یک گزینه را انتخاب کنید")
+        self.e_phoneNum.delete(0,END)
+        self.c_accountType.set("یک گزینه را انتخاب کنید")
+        self.e_personnelId.delete(0,END)
+        self.e_karmandPass.delete(0,END)
+        self.karmandImg['file']='image/imgSelectorBg.png'
+        self.e_nameKarmand.focus()
+
+        self.con=sql.connect('mydb.db')
+        self.cur=self.con.cursor()
+        self.data=(self.personnelId,self.pesonnelName,self.pesonnelLast,self.nationalCode,self.gender,self.phoneNum,self.accountType,self.personnelPass,self.photo)
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS karmand (id TEXT PRIMARY KEY NOT NULL ,name TEXT NOT NULL ,last_name TEXT NOT NULL,national_code TEXT NOT NULL
+        ,gender INTEGER NOT NULL,phone_number TEXT NOT NULL,account_type TEXT NOT NULL,personnel_pass TEXT NOT NULL,photo BLOB NOT NULL)''')
+        self.cur.execute('INSERT INTO karmand(id,name,last_name,national_code,gender,phone_number,account_type,personnel_pass,photo) VALUES(?,?,?,?,?,?,?,?,?)',self.data)
+        self.con.commit()
+        self.numlist=len(self.listKarmand.get_children())
+        self.listKarmand.insert(parent='',index='end',text='',values=(self.accountType,self.phoneNum,self.accountType,
+                                                                    self.pesonnelLast,self.pesonnelName,self.personnelId,self.numlist+1))
+
 
 O=A()
 O.mainloop()
